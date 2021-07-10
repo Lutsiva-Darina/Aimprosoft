1afaf2047c4366d107db1f9e71dfabfc29db2f34
Lutsiva-Darina <d.lutsiva@gmail.com>
2021-07-11 00:08:08 +0300

    menu down open

--- menu_down/menu.js
+++ menu_down/menu.js
@@ -1,46 +1,55 @@
-// var plus = document.querySelectorAll('.plus');
-// var submenu = document.querySelectorAll('.submenu');
-// plus.forEach(element => {
-//     element.addEventListener('click', (e) => {
-//         slideTo = e.target.getAttribute('submenu');
-//         console.log(slideTo);
-//     });
-// });
-// let nav = document.querySelectorAll('.nav');
-// nav.forEach(nav_elem => {
-//     nav_elem.addEventListener('click', function(event) {
-//         var target = event.target; //где был клик
-//         console.log(target);
-//         var s = target.getElementsByClassName('submenu')[0];
-//         console.log(s);
-//         // closeMenu();
-//         // s[0].style.display = "block";
+let submenu = document.querySelector('.submenu');
+let plus = document.querySelectorAll(".menu-item .plus");
 
-//     });
+let menuWrapper = document.querySelectorAll(".submenu .menu__wrapper");
+// menuWrapper.forEach(element => {
+//     console.log(element);
 // });
+let count = 0;
+plus.forEach(plus_el => {
+    plus_el.addEventListener('click', event => {
+        if(submenu.childNodes.length>0){
+            count++; 
+            console.log(count);
+        menuWrapper.forEach(menuWrapper_el => {
+            console.log(menuWrapper_el);
+           menuWrapper_el.classList.add("sub__wrapper");
+           let plus__stick = document.querySelectorAll(".menu-item .plus_vertical");
+           plus__stick.forEach(stick_el => {
+                if(count %2==0){
+                    submenu.style.display = "none";
+                    stick_el.style.display="block";
+                }
+                else{
+                    stick_el.style.display="none";
+                submenu.style.display = "block";
+                }
+           });
+           
+        });
+        
+        }
+      });
+     
+});
+// const button = document.querySelector('button');
+submenu.addEventListener('click', event => {
+  console.log("bur");
+});
+// if (submenu.childNodes.length > 0) { // Or just `if (element.childNodes.length)`
+//     plus.forEach(plus__element => {
+//         document.querySelector(".menu__burger_title").addEventListener('onclick', function() { console.log("Hi")});
+//     });
+// }
 
-let nav = document.getElementById('nav').onmouseover = function(event) {
-    var target = event.target; //где был клик
-    console.log(target);
-    if (target.className == 'menu__wrapper' || target.className == 'topmenu_block indent' || target.className == 'plus' || target.className == 'plus_vertical') {
-        console.log("name");
-        let s = target.getElementsByClassName('submenu')[0];
-        console.log(s);
-        // // closeMenu();
-        //     for (let i = 0; i < s.length; i++) {
-        //         console.log("jkjk");
-        //     }
-        // } else {
-        //     console.log("wrong");
-        // }
-    }
-};
-// nav.forEach(nav_elem => {
-//     nav_elem.addEventListener('click', function(event) {
-//         var target = event.target; //где был клик
-//         console.log(target);
-//         var s = target.getElementByClassName('submenu')[0];
-//         closeMenu();
-//         s[0].style.display = "block";
-//     });
-// });
+document.querySelector(".menu__burger_title").addEventListener('click', function() { submenu.style.backgroundColor="aqua"});
+// let menuWrapper = submenu.node.contains(".menu__wrapper");
+// if(submenu.contains(".menu__wrapper")){
+//     console.log("si");
+// }
+// let parent = document.querySelector('.submenu');
+
+// let contains = parent.contains(parent);
+// if(parent.contains(parent)){
+//     console.log("contains");
+// }
